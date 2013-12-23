58a28d014d6edcd51923f16ce586e4ca452d9f2a
Daniel Lindberg <dajoh10@gmail.com>
2013-12-23 21:21:04 +0100

    Updated Windows libraries and scripts to use VS2012.

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,4 +1,4 @@
 @echo off
 ThirdParty\Binaries\Premake4.exe --file=OpenSteamClient.lua clean
-ThirdParty\Binaries\Premake4.exe --file=OpenSteamClient.lua vs2010
+ThirdParty\Binaries\Premake4.exe --file=OpenSteamClient.lua vs2012
 pause
